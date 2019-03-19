d7984aecd0a7871859cb3456a996105195e13b87
Alexey <4pok45@mail.ru>
2019-03-19 18:08:53 +0300

    refactored

--- test_add_group.py
+++ test_add_group.py
@@ -1,53 +1,80 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time
+import unittest
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox(executable_path='C:/projects/drivers/geckodriver/geckodriver.exe')
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.katalon.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
-    def test_untitled_test_case(self):
-        driver = self.driver
+
+    def open_home_page(self, driver):
         driver.get("http://adressbook:81/group.php")
+
+    def login(self, driver, username, password):
+        # login
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+
+    def open_groups_page(self, driver):
         driver.find_element_by_name("new").click()
+
+    def create_group(self, driver, name, header, footer):
+        # init group creation
         driver.find_element_by_name("group_name").click()
+        # fill group form
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("sdadsadas")
+        driver.find_element_by_name("group_name").send_keys(name)
         driver.find_element_by_name("group_header").click()
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("dsadsadas")
+        driver.find_element_by_name("group_header").send_keys(header)
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("dsadsadsa")
+        driver.find_element_by_name("group_footer").send_keys(footer)
+        # submit group creation
         driver.find_element_by_name("submit").click()
+
+    def return_to_groups_page(self, driver):
         driver.find_element_by_link_text("group page").click()
+
+    def logout(self, driver):
         driver.find_element_by_link_text("Logout").click()
-    
+
+    def test_add_group(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, name="sdadsadas", header="dsadsadas", footer="dsadsadsa")
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+
+    def test_add_empty_group(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, name="", header="", footer="")
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
-    
+
     def is_alert_present(self):
         try: self.driver.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-    
+
     def close_alert_and_get_its_text(self):
         try:
             alert = self.driver.switch_to_alert()
@@ -58,7 +85,7 @@ class UntitledTestCase(unittest.TestCase):
                 alert.dismiss()
             return alert_text
         finally: self.accept_next_alert = True
-    
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
