c6d6f6cefbc06aa93a24935a0697aa7fee171b4d
Alexey <4pok45@mail.ru>
2019-03-25 16:36:52 +0300

    refactored

--- test_add_group.py
+++ test_add_group.py
@@ -10,17 +10,15 @@ class UntitledTestCase(unittest.TestCase):
         self.driver.implicitly_wait(30)
 
     def open_home_page(self, driver):
-        driver.get("http://adressbook:81/group.php")
+        driver.get("http://addressbook:81/group.php")
 
     def login(self, driver, username, password):
         # login
         driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys(username)
-        driver.find_element_by_name("pass").clear()
         driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+            ".//*[@id='LoginForm']/input[3]").click()
 
     def open_groups_page(self, driver):
         driver.find_element_by_name("new").click()
@@ -29,13 +27,10 @@ class UntitledTestCase(unittest.TestCase):
         # init group creation
         driver.find_element_by_name("group_name").click()
         # fill group form
-        driver.find_element_by_name("group_name").clear()
         driver.find_element_by_name("group_name").send_keys(name)
         driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
         driver.find_element_by_name("group_header").send_keys(header)
         driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
         driver.find_element_by_name("group_footer").send_keys(footer)
         # submit group creation
         driver.find_element_by_name("submit").click()
@@ -44,7 +39,7 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_link_text("group page").click()
 
     def logout(self, driver):
-        driver.find_element_by_link_text("Logout").click()
+        driver.find_element_by_link_text("Выйти").click()
 
     def test_add_group(self):
         driver = self.driver
@@ -64,31 +59,13 @@ class UntitledTestCase(unittest.TestCase):
         self.return_to_groups_page(driver)
         self.logout(driver)
 
-
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
 
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-
     def tearDown(self):
         self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
