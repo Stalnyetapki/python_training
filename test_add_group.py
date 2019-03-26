a9f9bfc95db693708854f01bbe2a41d3e564ac28
Alexey <4pok45@mail.ru>
2019-03-26 10:52:36 +0300

    refactored

--- test_add_group.py
+++ test_add_group.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 from group import Group
 
-class UntitledTestCase(unittest.TestCase):
+def is_alert_present(driver):
+    try:
+        driver.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class test_add_group(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox(executable_path='C:/projects/drivers/geckodriver/geckodriver.exe')
         self.driver.implicitly_wait(30)
@@ -15,6 +20,7 @@ class UntitledTestCase(unittest.TestCase):
 
     def login(self, driver, username, password):
         # login
+        self.open_home_page(driver)
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").send_keys(password)
@@ -25,6 +31,7 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_name("new").click()
 
     def create_group(self, driver, group):
+        self.open_groups_page(driver)
         # init group creation
         driver.find_element_by_name("group_name").click()
         # fill group form
@@ -35,6 +42,7 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         driver.find_element_by_name("submit").click()
+        self.return_to_groups_page(driver)
 
     def return_to_groups_page(self, driver):
         driver.find_element_by_link_text("group page").click()
@@ -44,26 +52,15 @@ class UntitledTestCase(unittest.TestCase):
 
     def test_add_group(self):
         driver = self.driver
-        self.open_home_page(driver)
         self.login(driver, username="admin", password="secret")
-        self.open_groups_page(driver)
         self.create_group(driver, Group(name="sdadsadas", header="dsadsadas", footer="dsadsadsa"))
-        self.return_to_groups_page(driver)
         self.logout(driver)
 
     def test_add_empty_group(self):
         driver = self.driver
-        self.open_home_page(driver)
         self.login(driver, username="admin", password="secret")
-        self.open_groups_page(driver)
         self.create_group(driver, Group(name="", header="", footer=""))
-        self.return_to_groups_page(driver)
         self.logout(driver)
-
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
 
     def tearDown(self):
         self.driver.quit()
