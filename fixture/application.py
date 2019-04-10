3a86829d6c5d29bb8c20e56e737797afeec61b99
Alexey <4pok45@mail.ru>
2019-04-10 19:49:31 +0300

    added precondition in tests

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ class Application:
 
     def __init__(self):
         self.driver = webdriver.Firefox(executable_path='C:/projects/drivers/geckodriver/geckodriver.exe')
-        self.driver.implicitly_wait(30)
+        self.driver.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
 
