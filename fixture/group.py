acefeaaedd6543aa87fd6f776482696eac0530fd
Alexey <4pok45@mail.ru>
2019-04-20 16:10:36 +0300

    added check in all test

--- fixture/group.py
+++ fixture/group.py
@@ -20,6 +20,7 @@ class GroupHelper:
         # submit group creation
         driver.find_element_by_name("submit").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def change_field_value(self, field_name, text):
         driver = self.app.driver
@@ -34,22 +35,31 @@ class GroupHelper:
         self.change_field_value("group_header", group.header)
         self.change_field_value("group_footer", group.footer)
 
-    def delete_first_group(self):
+    def delete_group_by_index(self, index):
         driver = self.app.driver
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         driver.find_element_by_name("delete").click()
         self.return_to_groups_page()
+        self.group_cache = None
+
+    def delete_first_group(self):
+        driver = self.app.driver
+        self.delete_group_by_index(0)
+
+    def select_group_by_index(self, index):
+        driver = self.app.driver
+        driver.find_elements_by_name("selected[]")[index].click()
 
     def select_first_group(self):
         driver = self.app.driver
         driver.find_element_by_name("selected[]").click()
 
-    def modify_first_group(self, new_group_data):
+    def modify_group_by_index(self, index, new_group_data):
         driver = self.app.driver
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open modification form
         driver.find_element_by_name("edit").click()
         # fill group form
@@ -57,6 +67,11 @@ class GroupHelper:
         # submit modification
         driver.find_element_by_name("update").click()
         self.return_to_groups_page()
+        self.group_cache = None
+
+    def modify_first_group(self):
+        driver = self.app.driver
+        self.modify_group_by_index(0)
 
     def return_to_groups_page(self):
         driver = self.app.driver
@@ -67,15 +82,18 @@ class GroupHelper:
         self.open_groups_page()
         return len(driver.find_elements_by_name("selected[]"))
 
+    group_cache = None
+
     def get_group_list(self):
-        driver = self.app.driver
-        self.open_groups_page()
-        groups = []
-        for element in driver.find_elements_by_css_selector("form>input[type='checkbox']"):
-            text = element.get_attribute("title")[8:-1]
-            id = element.get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            driver = self.app.driver
+            self.open_groups_page()
+            self.group_cache = []
+            for element in driver.find_elements_by_css_selector("form>input[type='checkbox']"):
+                text = element.get_attribute("title")[8:-1]
+                id = element.get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
 
 
 
--- fixture/session.py
+++ fixture/session.py
@@ -16,7 +16,7 @@ class SessionHelper:
         driver.find_element_by_name("pass").clear()
         driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_xpath(
-            ".//*[@id='LoginForm']/input[3]").click()
+            './/*[@id="LoginForm"]/input[3]').click()
 
     def logout(self):
         driver = self.app.driver
@@ -29,7 +29,11 @@ class SessionHelper:
 
     def is_logged_in_as(self, username):
         driver = self.app.driver
-        return driver.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        driver = self.app.driver
+        return driver.find_element_by_xpath("//div/div[1]/form/b").text[1:-1]
 
     def ensure_logout(self):
         driver = self.app.driver
