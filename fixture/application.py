249a478e8752d5135f3b8b81b0cd472f60388c9e
Alexey <4pok45@mail.ru>
2019-05-22 18:01:06 +0300

    refactored

--- fixture/application.py
+++ fixture/application.py
@@ -6,11 +6,20 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.driver = webdriver.Firefox(executable_path='C:/projects/drivers/geckodriver/geckodriver.exe')
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.driver = webdriver.Firefox()
+        elif browser == "chrome":
+            self.driver = webdriver.Ch
+            rome()
+        elif browser == "ie":
+            self.driver = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -21,7 +30,7 @@ class Application:
 
     def open_home_page(self):
         driver = self.driver
-        driver.get("http://addressbook:81")
+        driver.get(self.base_url)
 
     def destroy(self):
         self.driver.quit()
